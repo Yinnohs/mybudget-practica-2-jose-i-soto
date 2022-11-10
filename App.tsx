11a1812667739ffc8b06ff29032d416b9a8b87c7
josesoto <josesoto@itop.es>
2022-11-10 15:55:52 +0000

    feat(all): first commit for the proyect

--- App.tsx
+++ App.tsx
@@ -1,19 +1,27 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+         <Stack.Navigator>
+          <Stack.Screen
+          name='Home'
+          component={}
+          options={{title:"Hola"}}/>
+         </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ccc',
     alignItems: 'center',
     justifyContent: 'center',
   },
