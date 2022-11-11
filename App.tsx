4682a7a7aa3bbcc974c6cfe8d142c91de0c30f55
josesoto <josesoto@itop.es>
2022-11-11 09:31:40 +0000

    feat(app.tsx): added provider to app.tsx

--- App.tsx
+++ App.tsx
@@ -1,22 +1,29 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useFonts } from 'expo-font';
+import { appTheme, MovementProvider } from './src/constants';
 import { HomeView } from './src/views';
 
 const Stack = createNativeStackNavigator()
 
 export default function App() {
   const[texts] = useFonts({
-    "title": require("./assets/fonts/Poppins//Poppins-Medium.ttf"),
-    "normal": require("./assets/fonts/Roboto/Roboto-Regular.ttf")
+    [appTheme.textTitle]: require("./assets/fonts/Poppins//Poppins-Medium.ttf"),
+    [appTheme.textNormal]: require("./assets/fonts/Roboto/Roboto-Regular.ttf")
  })
   return (
     <NavigationContainer>
          <Stack.Navigator>
-          <Stack.Screen
-          name='Home'
-          component={HomeView}
-          options={{title:"Este es el home View"}}/>
+          <MovementProvider>
+            <Stack.Screen
+            name='Home'
+            component={HomeView}
+            options={{title:"Este es el home View"}}/>
+            <Stack.Screen
+            name='Detail:id'
+            component={HomeView}
+            options={{title:"Este es el home View"}}/>
+          </MovementProvider>
          </Stack.Navigator>
     </NavigationContainer>
   );
